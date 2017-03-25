91d9b0c6bdec0410139eb691b2af9e1cc3edc687
DLagos <dionmattlagos@gmail.com>
2017-03-24 21:50:36 -0700

    fixed ai

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,44 +4,21 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 
-	public Transform[] waypoints;
-	int cur = 0;
+	public Transform target;
 	UnityEngine.AI.NavMeshAgent enemyAgent;
-	float speed = 5.0f;
-	private Vector3 m_initialposition;
+	Vector3 destination; 
 
 	// Use this for initialization
 	void Start () {
-		m_initialposition = transform.position;
 		enemyAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		destination = enemyAgent.destination;
+		//target = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (transform.position != waypoints[cur].position) {
-			enemyAgent.destination = waypoints[cur].position;
-		}
-		else cur = (cur + 1) % waypoints.Length;
+		enemyAgent.destination = target.position;
 	}
-	/*
-	void WorldInteraction()
-	{
-		Ray rayInteraction = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit infoInteraction;
 
-		if (Physics.Raycast(rayInteraction, out infoInteraction, Mathf.Infinity))
-		{
-			GameObject objectInteraction = infoInteraction.collider.gameObject;
-			if(objectInteraction.tag == "Interactable Object")
-			{
-				// for objects to interact with
-			}
-			else
-			{
-				enemyAgent.destination = waypoints[cur].position;
 
-			}
-		}
-	}
-	*/
 }
